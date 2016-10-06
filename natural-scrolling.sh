99de007780c6fc9fb4a0a599e63f7c9ea0329966
Lars Gohr <larsgohr@gmail.com>
2016-10-06 15:07:12 +0200

    Fix to also have the scrolling via Scrolling Distance

--- natural-scrolling.sh
+++ natural-scrolling.sh
@@ -1,4 +1,10 @@
 for device in $(xinput list | grep 'Mouse\|Touchpad' | cut -d'=' -f2 | cut -d'[' -f1)
 do
-xinput -set-button-map $device 1 2 3 5 4
+distance=$(xinput list-props $device | grep -i "Evdev Scrolling Distance" | cut -d"(" -f2 | cut -d")" -f1)
+if [ -z "$distance" ]
+  then
+    xinput -set-button-map $device 1 2 3 5 4
+  else
+    xinput set-prop $device $distance -1 -1 -1
+fi
 done
