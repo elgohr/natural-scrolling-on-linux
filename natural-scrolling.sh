a20bf3cd3fa45f0a86ad363c00a03abf6f5c8578
Lars Gohr <larsgohr@gmail.com>
2018-07-31 09:32:48 +0200

    Updated to work on Mint Tara

--- natural-scrolling.sh
+++ natural-scrolling.sh
@@ -1,10 +1,4 @@
-for device in $(xinput list | grep 'Mouse\|Touchpad' | cut -d'=' -f2 | cut -d'[' -f1)
+for device in $(xinput list | grep 'Mouse' | cut -d'=' -f2 | cut -d'[' -f1)
 do
-distance=$(xinput list-props $device | grep -i "Evdev Scrolling Distance" | cut -d"(" -f2 | cut -d")" -f1)
-if [ -z "$distance" ]
-  then
-    xinput -set-button-map $device 1 2 3 5 4
-  else
-    xinput set-prop $device $distance -1 -1 -1
-fi
+ xinput --set-int-prop $device "libinput Natural Scrolling Enabled" 8 1
 done
